62f3d44f7d8408a78b998d230f171d784688a916
Roland Doll <rdoll007@gmail.com>
2022-08-29 11:15:42 -0400

    got it working again

---
+++ app/src/main/java/com/example/android_todoapp/CardFragment.kt
@@ -0,0 +1,6 @@
+package com.example.android_todoapp
+
+import androidx.fragment.app.Fragment
+
+class CardFragment : Fragment(R.layout.card) {
+}
--- app/src/main/java/com/example/android_todoapp/MainActivity.kt
+++ app/src/main/java/com/example/android_todoapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<HomeFragment>(R.id.fragment_container_view)
+                //add<CardFragment>(R.id.fragment_container_view)
             }
         }
     }
