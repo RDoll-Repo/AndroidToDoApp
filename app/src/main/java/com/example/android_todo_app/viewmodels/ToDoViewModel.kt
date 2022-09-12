55b709a566f537f97e26902194572a44d2eaa564
Roland Doll <rdoll007@gmail.com>
2022-09-12 13:45:47 -0400

    start viewmodel

---
+++ app/src/main/java/com/example/android_todo_app/viewmodels/ToDoViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.android_todo_app.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android_todo_app.models.ToDo
+
+class ToDoViewModel: ViewModel() {
+    var list = MutableLiveData<ArrayList<ToDo>>()
+    var newList = arrayListOf<ToDo>()
+
+    fun addToDo(newToDo: ToDo) {
+        newList.add(newToDo)
+        list.value = newList
+    }
+
+    fun removeToDo(toDo:ToDo) {
+        newList.remove(toDo)
+        list.value = newList
+    }
+
+    fun updateToDo(toDo:ToDo) {
+        newList.
+    }
+}
